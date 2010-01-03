8fd12878605e4eca9f337831abdb9ffd2c1339cd
unknown <Gilbert@.(none)>
2010-01-03 18:20:21 +0800

    hospital_rec.sql file updated

--- hospital_rec.sql
+++ hospital_rec.sql
@@ -2,33 +2,33 @@
 -- version 3.2.0.1
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Jan 03, 2010 at 07:32 AM
--- Server version: 5.1.37
--- PHP Version: 5.3.0
+-- ??: localhost
+-- ????: Jan 03, 2010, 10:19 AM
+-- ?????: 5.1.37
+-- PHP ??: 5.3.0
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 --
--- Database: `hospital_rec`
+-- ???: `hospital_rec`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `allergy`
+-- ?????: `allergy`
 --
 
 DROP TABLE IF EXISTS `allergy`;
 CREATE TABLE IF NOT EXISTS `allergy` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` text NOT NULL,
-  `description` text,
+  `name` text COLLATE utf8_unicode_ci NOT NULL,
+  `description` text COLLATE utf8_unicode_ci,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
 
 --
--- Dumping data for table `allergy`
+-- ??????????: `allergy`
 --
 
 INSERT INTO `allergy` (`id`, `name`, `description`) VALUES
@@ -38,7 +38,7 @@ INSERT INTO `allergy` (`id`, `name`, `description`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `dia-allergy_rec`
+-- ?????: `dia-allergy_rec`
 --
 
 DROP TABLE IF EXISTS `dia-allergy_rec`;
@@ -47,17 +47,17 @@ CREATE TABLE IF NOT EXISTS `dia-allergy_rec` (
   `pat_id` int(11) NOT NULL,
   `allergy_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
 --
--- Dumping data for table `dia-allergy_rec`
+-- ??????????: `dia-allergy_rec`
 --
 
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `dia-disease_rec`
+-- ?????: `dia-disease_rec`
 --
 
 DROP TABLE IF EXISTS `dia-disease_rec`;
@@ -66,29 +66,29 @@ CREATE TABLE IF NOT EXISTS `dia-disease_rec` (
   `pat_id` int(11) NOT NULL,
   `dis_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
 --
--- Dumping data for table `dia-disease_rec`
+-- ??????????: `dia-disease_rec`
 --
 
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `disease`
+-- ?????: `disease`
 --
 
 DROP TABLE IF EXISTS `disease`;
 CREATE TABLE IF NOT EXISTS `disease` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` text NOT NULL,
-  `description` text,
+  `name` text COLLATE utf8_unicode_ci NOT NULL,
+  `description` text COLLATE utf8_unicode_ci,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
 
 --
--- Dumping data for table `disease`
+-- ??????????: `disease`
 --
 
 INSERT INTO `disease` (`id`, `name`, `description`) VALUES
@@ -98,19 +98,19 @@ INSERT INTO `disease` (`id`, `name`, `description`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `medicine`
+-- ?????: `medicine`
 --
 
 DROP TABLE IF EXISTS `medicine`;
 CREATE TABLE IF NOT EXISTS `medicine` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` text NOT NULL,
-  `description` text,
+  `name` text COLLATE utf8_unicode_ci NOT NULL,
+  `description` text COLLATE utf8_unicode_ci,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
 
 --
--- Dumping data for table `medicine`
+-- ??????????: `medicine`
 --
 
 INSERT INTO `medicine` (`id`, `name`, `description`) VALUES
@@ -120,50 +120,50 @@ INSERT INTO `medicine` (`id`, `name`, `description`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `patient_personal`
+-- ?????: `patient_personal`
 --
 
 DROP TABLE IF EXISTS `patient_personal`;
 CREATE TABLE IF NOT EXISTS `patient_personal` (
   `pid` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
-  `gender` varchar(1) NOT NULL,
-  `address` text NOT NULL,
-  `contact_no` varchar(8) NOT NULL,
+  `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `gender` varchar(1) COLLATE utf8_unicode_ci NOT NULL,
+  `address` text COLLATE utf8_unicode_ci NOT NULL,
+  `contact_no` varchar(8) COLLATE utf8_unicode_ci NOT NULL,
   `birthday` date NOT NULL,
   `pic` int(11) NOT NULL,
   `last_diagnosis` datetime NOT NULL,
-  `description` text,
+  `description` text CHARACTER SET latin1,
   PRIMARY KEY (`pid`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
 --
--- Dumping data for table `patient_personal`
+-- ??????????: `patient_personal`
 --
 
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `privilege`
+-- ?????: `privilege`
 --
 
 DROP TABLE IF EXISTS `privilege`;
 CREATE TABLE IF NOT EXISTS `privilege` (
-  `Role` varchar(25) NOT NULL,
+  `Role` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
   `Read` tinyint(1) NOT NULL,
   PRIMARY KEY (`Role`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Dumping data for table `privilege`
+-- ??????????: `privilege`
 --
 
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `t-m_rec`
+-- ?????: `t-m_rec`
 --
 
 DROP TABLE IF EXISTS `t-m_rec`;
@@ -172,17 +172,17 @@ CREATE TABLE IF NOT EXISTS `t-m_rec` (
   `tid` int(11) NOT NULL,
   `mid` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
 --
--- Dumping data for table `t-m_rec`
+-- ??????????: `t-m_rec`
 --
 
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `treatment`
+-- ?????: `treatment`
 --
 
 DROP TABLE IF EXISTS `treatment`;
@@ -190,36 +190,42 @@ CREATE TABLE IF NOT EXISTS `treatment` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `pid` int(11) NOT NULL,
   `pic` int(11) NOT NULL,
-  `description` text,
+  `description` text COLLATE utf8_unicode_ci,
   `date_of_issue` date NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
 --
--- Dumping data for table `treatment`
+-- ??????????: `treatment`
 --
 
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- ?????: `user`
 --
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
   `uid` int(11) NOT NULL AUTO_INCREMENT,
-  `Role` varchar(25) NOT NULL,
-  `pub_key` text NOT NULL,
-  `pwd` text NOT NULL,
+  `Role` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
+  `pub_key` text COLLATE utf8_unicode_ci NOT NULL,
+  `mod` text COLLATE utf8_unicode_ci,
+  `pwd` text COLLATE utf8_unicode_ci NOT NULL,
   `RegDate` date NOT NULL,
   PRIMARY KEY (`uid`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=10 ;
 
 --
--- Dumping data for table `user`
+-- ??????????: `user`
 --
 
-INSERT INTO `user` (`uid`, `Role`, `pub_key`, `pwd`, `RegDate`) VALUES
-(1, 'doc', '10001', 'G5XuytlF', '2010-01-02'),
-(2, 'hi', '10001', 'CAhbMZgt', '2010-01-02');
+INSERT INTO `user` (`uid`, `Role`, `pub_key`, `mod`, `pwd`, `RegDate`) VALUES
+(1, 'doc', '10001', NULL, 'G5XuytlF', '2010-01-02'),
+(2, 'hi', '10001', NULL, 'CAhbMZgt', '2010-01-02'),
+(5, '1', '10001', '9ec1d3c7fead0cc7562d2fbc845146a10f09fc06f78aba3345e4d5888af680ea30859233e82980caf9ef9da127433f4d3bf80feda93dae04293e8e4263abbfdd6ccf1a7a4cd3a6442f952e174ae5eabcca378dc6b96373c6450adcb8eb27dc75082eb480fbf269092935bc71cf48bf1f88eacbe73df3e979b867e833c8665541', '1234', '2010-01-03'),
+(6, '1', '10001', 'b18bf734801f83038c0246f8fac2c888e5e20f7f204bd38de35af6d7f0cefaafb3279a5753c2656830a8b681a3c06596a7e2080313a5b9c959eec2c63eee8c7c87feb5a9160f47a70267c4d9d623d335581dfdb36ebc6be31b9921e151e957cee7279ff9cfdfb21cf71ccacd65e5d35e44617657165a6d7b0c693ab78985cc33', 'vQ3D1RhF', '2010-01-03'),
+(7, '1', '10001', 'b18bf734801f83038c0246f8fac2c888e5e20f7f204bd38de35af6d7f0cefaafb3279a5753c2656830a8b681a3c06596a7e2080313a5b9c959eec2c63eee8c7c87feb5a9160f47a70267c4d9d623d335581dfdb36ebc6be31b9921e151e957cee7279ff9cfdfb21cf71ccacd65e5d35e44617657165a6d7b0c693ab78985cc33', 'vQ3D1RhF', '2010-01-03'),
+(8, '1', '10001', 'b18bf734801f83038c0246f8fac2c888e5e20f7f204bd38de35af6d7f0cefaafb3279a5753c2656830a8b681a3c06596a7e2080313a5b9c959eec2c63eee8c7c87feb5a9160f47a70267c4d9d623d335581dfdb36ebc6be31b9921e151e957cee7279ff9cfdfb21cf71ccacd65e5d35e44617657165a6d7b0c693ab78985cc33', 'vQ3D1RhF', '2010-01-03'),
+(9, '1', '10001', 'b729cd7593d9474d12e75a125cddc059edfd6c39a6a9864f9e186b4992499e8f7ed9dbb6dbda2c9e7b111596cc58c5969226ff713f19907acb40667e0737c60f08f9fedf6a4a22f56b804b6d83ee8d03f0dc75a8f87013b2c278dcad650c2f2800c1610e7ca749d7a0dcceb54456355cac2bf23acfc471fe4749772898e27f0d', '8783bax8', '2010-01-03');
